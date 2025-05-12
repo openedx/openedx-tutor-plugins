9080eb5b19b37358504593861b10b3f29961b456
Alejandro Cardenas <alecar.main@gmail.com>
2025-05-12 16:31:24 -0500

    feat: create theme folders from config and document structure

--- plugins/tutor-contrib-paragon/README.rst
+++ plugins/tutor-contrib-paragon/README.rst
@@ -1,8 +1,9 @@
-paragon plugin for `Tutor <https://docs.tutor.edly.io>`__
-#########################################################
+Paragon plugin for `Tutor <https://docs.tutor.edly.io>`__
+###########################################################
 
-Facilitates the generation and static hosting of Paragon-based theme CSS files for Open edX MFEs using Tutor.
+Facilitates the generation and static hosting of Paragon-based CSS themes for Open edX Micro-Frontend (MFE) applications using `Paragon <https://openedx.github.io/paragon/>`__.
 
+This plugin provides a local folder structure to manage theme source files and compile them into CSS, enabling flexible customization of Open edX MFEs via Tutor.
 
 Installation
 ************
@@ -11,13 +12,50 @@ Installation
 
     pip install git+https://github.com/openedx/openedx-tutor-plugins.git#subdirectory=plugins/tutor-contrib-paragon
 
-Usage
-*****
+For development:
+
+.. code-block:: bash
+
+    cd openedx-tutor-plugins/plugins/tutor-contrib-paragon
+    pip install -e .
+
+Enable the plugin:
 
 .. code-block:: bash
 
     tutor plugins enable paragon
 
+Directory Structure
+*******************
+
+The plugin will create the following structure inside your Tutor environment:
+
+.. code-block::
+
+    tutor/env/plugins/paragon/
+    ├── theme-sources/           # Place your style dictionary folders here (e.g. theme-xyz/)
+    └── compiled-themes/         # Compiled CSS files go here, ready for static hosting
+
+Each theme inside `theme-sources/` should follow the Style Dictionary folder structure.
+Only themes listed in `PARAGON_ENABLED_THEMES` will be compiled.
+
+The `compiled-themes/` folder is designed to be exposed via static web servers such as **nginx** or **Caddy**, or can be uploaded to cloud hosting providers like **Amazon S3** or **Cloudflare Pages**.
+
+Configuration
+*************
+
+All configuration variables can be overridden via `tutor config save`:
+
+.. code-block:: yaml
+
+    PARAGON_THEME_SOURCES_PATH: "env/plugins/paragon/theme-sources"
+    PARAGON_COMPILED_THEMES_PATH: "env/plugins/paragon/compiled-themes"
+    PARAGON_ENABLED_THEMES:
+      - theme-1
+      - theme-2
+    PARAGON_SERVE_COMPILED_THEMES: true
+
+You may customize paths or theme names to suit your deployment.
 
 License
 *******
--- plugins/tutor-contrib-paragon/tutorparagon/plugin.py
+++ plugins/tutor-contrib-paragon/tutorparagon/plugin.py
@@ -4,6 +4,7 @@ from glob import glob
 import click
 import importlib_resources
 from tutor import hooks
+from tutor import config as tutor_config
 
 from .__about__ import __version__
 
@@ -13,112 +14,45 @@ from .__about__ import __version__
 
 hooks.Filters.CONFIG_DEFAULTS.add_items(
     [
-        # Add your new settings that have default values here.
-        # Each new setting is a pair: (setting_name, default_value).
-        # Prefix your setting names with 'PARAGON_'.
+        # Plugin version (used for compatibility or debugging)
         ("PARAGON_VERSION", __version__),
-    ]
-)
-
-hooks.Filters.CONFIG_UNIQUE.add_items(
-    [
-        # Add settings that don't have a reasonable default for all users here.
-        # For instance: passwords, secret keys, etc.
-        # Each new setting is a pair: (setting_name, unique_generated_value).
-        # Prefix your setting names with 'PARAGON_'.
-        # For example:
-        ### ("PARAGON_SECRET_KEY", "{{ 24|random_string }}"),
-    ]
-)
-
-hooks.Filters.CONFIG_OVERRIDES.add_items(
-    [
-        # Danger zone!
-        # Add values to override settings from Tutor core or other plugins here.
-        # Each override is a pair: (setting_name, new_value). For example:
-        ### ("PLATFORM_NAME", "My platform"),
+        # Directory where users will place style-dictionary source files
+        # Each subfolder inside will represent a theme (e.g., "theme-abc/")
+        ("PARAGON_THEME_SOURCES_PATH", "env/plugins/paragon/theme-sources"),
+        # Directory where compiled CSS themes will be stored after transformation
+        # One subfolder per compiled theme (e.g., "theme-abc/core.min.css")
+        ("PARAGON_COMPILED_THEMES_PATH", "env/plugins/paragon/compiled-themes"),
+        # List of enabled themes to compile and serve
+        # Only themes listed here will be processed, even if others exist in sources
+        ("PARAGON_ENABLED_THEMES", []),
+        # Whether Tutor should expose the compiled themes to be served (e.g. via nginx, cady or static server)
+        ("PARAGON_SERVE_COMPILED_THEMES", True),
     ]
 )
 
 
-########################################
-# INITIALIZATION TASKS
-########################################
+# Create directories for build and host
+@hooks.Actions.PROJECT_ROOT_READY.add()
+def create_paragon_folders(project_root: str) -> None:
+    config = tutor_config.load(project_root)
 
-# To add a custom initialization task, create a bash script template under:
-# tutorparagon/templates/paragon/tasks/
-# and then add it to the MY_INIT_TASKS list. Each task is in the format:
-# ("<service>", ("<path>", "<to>", "<script>", "<template>"))
-MY_INIT_TASKS: list[tuple[str, tuple[str, ...]]] = [
-    # For example, to add LMS initialization steps, you could add the script template at:
-    # tutorparagon/templates/paragon/tasks/lms/init.sh
-    # And then add the line:
-    ### ("lms", ("paragon", "tasks", "lms", "init.sh")),
-]
-
-
-# For each task added to MY_INIT_TASKS, we load the task template
-# and add it to the CLI_DO_INIT_TASKS filter, which tells Tutor to
-# run it as part of the `init` job.
-for service, template_path in MY_INIT_TASKS:
-    full_path: str = str(
-        importlib_resources.files("tutorparagon")
-        / os.path.join("templates", *template_path)
+    # Paths from config (always have defaults)
+    theme_sources_path = os.path.join(
+        project_root, str(config["PARAGON_THEME_SOURCES_PATH"])
     )
-    with open(full_path, encoding="utf-8") as init_task_file:
-        init_task: str = init_task_file.read()
-    hooks.Filters.CLI_DO_INIT_TASKS.add_item((service, init_task))
+    compiled_themes_path = os.path.join(
+        project_root, str(config["PARAGON_COMPILED_THEMES_PATH"])
+    )
 
-
-########################################
-# DOCKER IMAGE MANAGEMENT
-########################################
-
-
-# Images to be built by `tutor images build`.
-# Each item is a quadruple in the form:
-#     ("<tutor_image_name>", ("path", "to", "build", "dir"), "<docker_image_tag>", "<build_args>")
-hooks.Filters.IMAGES_BUILD.add_items(
-    [
-        # To build `myimage` with `tutor images build myimage`,
-        # you would add a Dockerfile to templates/paragon/build/myimage,
-        # and then write:
-        ### (
-        ###     "myimage",
-        ###     ("plugins", "paragon", "build", "myimage"),
-        ###     "docker.io/myimage:{{ PARAGON_VERSION }}",
-        ###     (),
-        ### ),
-    ]
-)
-
-
-# Images to be pulled as part of `tutor images pull`.
-# Each item is a pair in the form:
-#     ("<tutor_image_name>", "<docker_image_tag>")
-hooks.Filters.IMAGES_PULL.add_items(
-    [
-        # To pull `myimage` with `tutor images pull myimage`, you would write:
-        ### (
-        ###     "myimage",
-        ###     "docker.io/myimage:{{ PARAGON_VERSION }}",
-        ### ),
-    ]
-)
-
-
-# Images to be pushed as part of `tutor images push`.
-# Each item is a pair in the form:
-#     ("<tutor_image_name>", "<docker_image_tag>")
-hooks.Filters.IMAGES_PUSH.add_items(
-    [
-        # To push `myimage` with `tutor images push myimage`, you would write:
-        ### (
-        ###     "myimage",
-        ###     "docker.io/myimage:{{ PARAGON_VERSION }}",
-        ### ),
-    ]
-)
+    for path, label in [
+        (theme_sources_path, "Theme Shources"),
+        (compiled_themes_path, "Compiled Themes"),
+    ]:
+        if os.path.exists(path):
+            print(f"[paragon] {label} folder already exists at: {path}")
+        else:
+            os.makedirs(path, exist_ok=True)
+            print(f"[paragon] Created {label} folder at: {path}")
 
 
 ########################################
@@ -158,70 +92,3 @@ hooks.Filters.ENV_TEMPLATE_TARGETS.add_items(
 for path in glob(str(importlib_resources.files("tutorparagon") / "patches" / "*")):
     with open(path, encoding="utf-8") as patch_file:
         hooks.Filters.ENV_PATCHES.add_item((os.path.basename(path), patch_file.read()))
-
-
-########################################
-# CUSTOM JOBS (a.k.a. "do-commands")
-########################################
-
-# A job is a set of tasks, each of which run inside a certain container.
-# Jobs are invoked using the `do` command, for example: `tutor local do importdemocourse`.
-# A few jobs are built in to Tutor, such as `init` and `createuser`.
-# You can also add your own custom jobs:
-
-
-# To add a custom job, define a Click command that returns a list of tasks,
-# where each task is a pair in the form ("<service>", "<shell_command>").
-# For example:
-### @click.command()
-### @click.option("-n", "--name", default="plugin developer")
-### def say_hi(name: str) -> list[tuple[str, str]]:
-###     """
-###     An example job that just prints 'hello' from within both LMS and CMS.
-###     """
-###     return [
-###         ("lms", f"echo 'Hello from LMS, {name}!'"),
-###         ("cms", f"echo 'Hello from CMS, {name}!'"),
-###     ]
-
-
-# Then, add the command function to CLI_DO_COMMANDS:
-## hooks.Filters.CLI_DO_COMMANDS.add_item(say_hi)
-
-# Now, you can run your job like this:
-#   $ tutor local do say-hi --name="Alejandro Cardenas"
-
-
-#######################################
-# CUSTOM CLI COMMANDS
-#######################################
-
-# Your plugin can also add custom commands directly to the Tutor CLI.
-# These commands are run directly on the user's host computer
-# (unlike jobs, which are run in containers).
-
-# To define a command group for your plugin, you would define a Click
-# group and then add it to CLI_COMMANDS:
-
-
-### @click.group()
-### def paragon() -> None:
-###     pass
-
-
-### hooks.Filters.CLI_COMMANDS.add_item(paragon)
-
-
-# Then, you would add subcommands directly to the Click group, for example:
-
-
-### @paragon.command()
-### def example_command() -> None:
-###     """
-###     This is helptext for an example command.
-###     """
-###     print("You've run an example command.")
-
-
-# This would allow you to run:
-#   $ tutor paragon example-command
